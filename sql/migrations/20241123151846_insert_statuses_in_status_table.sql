9e4a2cb605edd235b3705b69405e8687c279d410
Allen <allendelmo123@gmail.com>
2024-11-23 19:20:25 +0300

    feat: SQL migration insert table in statuses

--- sql/migrations/20241123151846_insert_statuses_in_status_table.sql
+++ sql/migrations/20241123151846_insert_statuses_in_status_table.sql
@@ -1,9 +1,38 @@
 -- +goose Up
--- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
+INSERT INTO STATUSES(
+    ID,
+    NAME
+)
+VALUES(
+    gen_random_uuid(),
+    'PENDING'
+);
+
+INSERT INTO STATUSES(
+    ID,
+    NAME
+)
+VALUES(
+    gen_random_uuid(),
+    'READY TO COLLECT'
+);
+
+INSERT INTO STATUSES(
+    ID,
+    NAME
+)
+VALUES(
+    gen_random_uuid(),
+    'CANCELLED'
+);
+
+INSERT INTO STATUSES(
+    ID,
+    NAME
+)
+VALUES(
+    gen_random_uuid(),
+    'PARTIALLY DISPENSED'
+);
 
 -- +goose Down
--- +goose StatementBegin
-SELECT 'down SQL query';
--- +goose StatementEnd
